b4e34549e82c99cbf9f3876a16b45054e766eb60
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2020-08-28 11:59:49 +0430

    Robofriends app first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import App from './containers/App';
+import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
+
+const logger = createLogger(); 
+const rootReducer = combineReducers({ searchRobots, requestRobots});
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+  <Fragment>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Fragment>
+  ,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
